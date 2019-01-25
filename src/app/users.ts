45d307cbbcc4d7bc44dcd2b3cf1db30fcf3d2473
Mugo-Ndungu <twinnymugo@gmail.com>
2019-01-25 08:15:35 +0300

    Add class user.ts file

---
+++ src/app/users.ts
@@ -0,0 +1,6 @@
+export class Users {
+  constructor(public username: string, public image: string, public numFollowers: string,
+     public numFollowing: string,
+      public repo: string, public names: string, public email: string,
+       public reposite: string, public time: Date) {}
+}
